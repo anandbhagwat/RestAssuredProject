6722c30772f56021f3c9d3abf3771ac321d00a77
Anand Bhagwat <anandbhagwatdevops@gmail.com>
2019-03-02 20:20:18 +0530

    second commit

--- src/test/java/RestAssuredProject/RestAssuredProject/RestAssured_Program_6_AddGooglePlace_Element3.java
+++ src/test/java/RestAssuredProject/RestAssuredProject/RestAssured_Program_6_AddGooglePlace_Element3.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import static org.hamcrest.Matchers.equalTo;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
@@ -12,8 +14,8 @@ public class RestAssured_Program_6_AddGooglePlace_Element3 {
   @Test
   public void RestAssured_Program() {
 	  RestAssured.baseURI="http://216.10.245.166";
-	  
-	  given().
+	  //Storing response in response
+	 Response resposneOutput= given().
 	  		queryParam("key","AIzaSyCo4aImrLRZQCNUSWQbFiFdRcZuY2QCnEM").
 	  		body("{"+
 
@@ -41,8 +43,45 @@ public class RestAssured_Program_6_AddGooglePlace_Element3 {
 
 	  		"}").
 	  		when().
-	  		post("/maps/api/place/add/json").andReturn();
-	  
+	  		post("/maps/api/place/add/json").
+	  		then().assertThat().statusCode(200).and().body("status",equalTo("OK")).
+	  		extract().response();
+	  		
+	 		// We got response in raw format, hence converting raw response into string
+	 		String respsonseAsString = resposneOutput.asString();
+	 		System.out.println(respsonseAsString);
+	 		//Then converting string into JSON response to traverse for specific element
+	 		JsonPath js = new JsonPath(respsonseAsString);
+	 		System.out.println(js.get("place_id"));
+	 		
+	 		String place_id = js.get("place_id");
+	 		
+	 		//Deleting the place with captured place _ id
+	 		
+	 		
+	 		
+	 		
+	 		given().
+	 				queryParam("key","AIzaSyCo4aImrLRZQCNUSWQbFiFdRcZuY2QCnEM").
+	 		body("{"+
+	 		    "\"place_id\":\""+place_id+"\""+
+	 		"}"
+).
+	 		when().
+	 		post("/maps/api/place/delete/json").
+	 		then().assertThat().statusCode(200);
+	 		
+	 		
+	 		
+	 		
+	 		
+	 		
+	 		
+	 		
+	 		
+	 		
+	 		
+	 		
   }
   
 }
